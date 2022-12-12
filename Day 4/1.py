07b65ef265ef6728ce20f128f454ea22a898cf26
Salam Hani <salamhani00@gmail.com>
2022-12-12 09:02:39 +0100

    Update 1.py

--- Day 4/1.py
+++ Day 4/1.py
@@ -2,10 +2,33 @@
 # Day 4 Assignment 1
 #
 
-import sys, os, string
+import sys, os
 def main():
+    sum = 0
     with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
         contents = f.readlines()
-        print(contents)
-        
+        for i in contents:
+            i = i.replace("-",",")
+            a, b, c, d = i.split(",")
+            A = range(int(a), int(b)+1)
+            B = range(int(c), int(d)+1)
+            a = contains(A, B)
+            b = contains(B, A)
+            if a == True or b == True:
+                sum += 1
+        print(sum)
+
+def contains(a:list, b:list):
+    result = True
+    for i in a:
+        if result == True:
+            for j in b:
+                if i == j:
+                    result = True
+                    break
+                else:
+                    result = False
+        else:
+            return False
+    return result
 main()
