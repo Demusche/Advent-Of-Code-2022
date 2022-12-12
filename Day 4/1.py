a273cf2483da975508c8b70c861b8a5b96cf63e0
Salam Hani <salamhani00@gmail.com>
2022-12-12 08:15:59 +0100

    Day 4

---
+++ Day 4/1.py
@@ -0,0 +1,11 @@
+# Advent of Code
+# Day 4 Assignment 1
+#
+
+import sys, os, string
+def main():
+    with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
+        contents = f.readlines()
+        print(contents)
+        
+main()
