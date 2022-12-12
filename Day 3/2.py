3444a0d605a70ada05a4ff5f8f2f873578675e19
Salam Hani <salamhani00@gmail.com>
2022-12-12 07:55:42 +0100

    Finished Day 3

--- Day 3/2.py
+++ Day 3/2.py
@@ -6,8 +6,22 @@ import sys, os, string
 def main():
     letters = list(string.ascii_letters)
     numbers = list(range(1, 53))
+    sum = 0
     with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
         contents = f.readlines()
         numberofletters_A = [0]*len(letters)
         numberofletters_B = [0]*len(letters)
         numberofletters_C = [0]*len(letters)
+        for i in range(0, len(contents), 3):
+            index = 0
+            for j in letters:
+                numberofletters_A[index] = contents[i].count(j)
+                numberofletters_B[index] = contents[i+1].count(j)
+                numberofletters_C[index] = contents[i+2].count(j)
+                if numberofletters_A[index] != 0 and numberofletters_B[index] != 0 and numberofletters_C[index] != 0:
+                    print('common letter is: ' + letters[index])
+                    sum += numbers[index]
+                    break
+                index += 1
+    print(sum)
+main()
